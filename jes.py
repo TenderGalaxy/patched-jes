3fe11c8cd398d7cbbd5a22b5fe63ab333035c94d
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2026-06-30 21:39:55 -0700

    Implement screen dimension input and UI scaling
    Added screen dimension input and adjusted UI scaling.

--- jes.py
+++ jes.py
@@ -4,6 +4,9 @@ from jes_ui import UI
 c_input = input("How many creatures do you want?\n100: Lightweight\n250: Standard (if you don't type anything, I'll go with this)\n500: Strenuous (this is what my carykh video used)\n")
 if c_input == "":
     c_input = "250"
+sc_input = input("What are the dimensions of the screen? Answer in form XxY (e.g. 1920x1078).\n1920x1078 is the default if you do not answer (These are the stablest dimensions).")
+
+screen = [1920, 1078]
 
 # Simulation
 # population size is 250 here, because that runs faster. You can increase it to 500 to replicate what was in my video, but do that at your own risk!
@@ -20,10 +23,12 @@ _mutation_rate=0.07, _big_mutation_rate=0.025,
 _UNITS_PER_METER=0.05)
 
 # Cosmetic UI variables
-ui = UI(_W_W=1920, _W_H=1078, _MOVIE_SINGLE_DIM=(650,650),
-_GRAPH_COOR=(850,50,900,500), _SAC_COOR=(850,560,900,300), _GENEALOGY_COOR=(20,105,530,802,42),
-_COLUMN_MARGIN=330, _MOSAIC_DIM=[10,24,24,30], #_MOSAIC_DIM=[10,10,17,22],
-_MENU_TEXT_UP=180, _CM_MARGIN1=20, _CM_MARGIN2=1)
+ratio = [screen[0]/1920, screen[1]/1078]
+ui = UI(_W_W=1920 * ratio[0], _W_H=1078 * ratio[1], _MOVIE_SINGLE_DIM=(650*ratio[0],650*ratio[1]),
+_GRAPH_COOR=(850*ratio[0],50*ratio[1],900*ratio[0],500*ratio[1]), _SAC_COOR=(850*ratio[0],560*ratio[1],900*ratio[0],300*ratio[1]),
+_GENEALOGY_COOR=(20,105*ratio[0],530*ratio[1],802*ratio[0],42*ratio[1]),
+_COLUMN_MARGIN=330*ratio[0], _MOSAIC_DIM=[10*ratio[0],24*ratio[1],24*ratio[0],30*ratio[1]], #_MOSAIC_DIM=[10,10,17,22],
+_MENU_TEXT_UP=180*ratio[1], _CM_MARGIN1=20*ratio[1], _CM_MARGIN2=1, RATIO=ratio)
 
 sim.ui = ui
 ui.sim = sim
